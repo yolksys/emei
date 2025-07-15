060ec56362d4e3e4823d9043b2e3b34ef43f210c
yolksys <cyf.signup@gmail.com>
2025-07-15 02:13:27 +0000

    Update main.go

--- examples/temp/main.go
+++ examples/temp/main.go
@@ -8,21 +8,6 @@ func main() {
   emei.RegRecver(_sssss)
   emei.Serve()
   return
-  // k := &Arith{}
-  // svr.RegRecv(k)
-  // svr.Serve()
-  // time.Sleep(time.Second * 5)
-  // svr.Wait()
-
-  // defer func() {
-  //   if false {
-  //     return
-  //   }
-  //   if r := recover(); r != nil {
-  //     fmt.Printf("Error: %+v", string(debug.Stack()))
-  //   }
-  //   //  recover()
-  // }()
 }
 
 func init() {
